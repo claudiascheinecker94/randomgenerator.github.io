dbefc1f3528565d65ea117e03c5f30e60846ad22
claudiascheinecker94 <116496696+claudiascheinecker94@users.noreply.github.com>
2022-11-27 12:48:05 +0000

    hide button

--- RandomNumber.js
+++ RandomNumber.js
@@ -1,6 +1,9 @@
 let randomNumbers=[];
 var userInput;
 var sum = 0;
+function hideButton(){
+    document.getElementById("button").style.visibility='hidden';
+}
 function go(){
     userInput = (prompt("Please provide a number between 1 and 10."));
     while (userInput.length == 0){
@@ -17,5 +20,6 @@ function go(){
         sum+=1;
         }
     }
+    hideButton();
     document.getElementById("main").innerHTML+="The generated random numbers are "+randomNumbers+". You have chosen number " + userInput + ". This number is present " +sum+ " time(s) within the array.<br />";
 }
